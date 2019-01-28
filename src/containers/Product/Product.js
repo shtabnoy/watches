a9d52db503f4434b55e3a9a4dbd821e7652d6e16
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2019-01-28 21:55:58 +0100

    style product

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -3,6 +3,26 @@ import PropTypes from 'prop-types'
 import api from 'lib/api'
 import ProductType from 'types/Product'
 import formatPrice from 'lib/formatPrice'
+import styled from '@emotion/styled'
+import colors from 'theme/colors'
+
+const ProductWrapper = styled.div`
+  margin: 8px 16px;
+  background-color: ${colors.pewter};
+  color: white;
+`
+
+const ProductField = styled.div`
+  display: flex;
+  justify-content: space-between;
+  &:nth-of-type(odd) {
+    background-color: ${colors.nevada};
+  }
+  & > div {
+    width: 50%;
+    padding: 16px;
+  }
+`
 
 class Product extends Component {
   static propTypes = {
@@ -45,24 +65,42 @@ class Product extends Component {
   }
 
   render() {
-    const { product = {} } = this.props
-    // console.log(product)
+    const { product } = this.props
     return (
       <>
-        <h1>{product.key}</h1>
-        <div>
-          <img src="#" alt="Product image" />
-          <div>{this.getValue(product, 'sku')}</div>
-          <div>{this.getValue(product, 'description')}</div>
-          {/* <div>
-            {formatPrice(
-              this.getValue(product, 'price').value,
-              this.getValue(product, 'price').unitAbbreviation
-            )}
-          </div> */}
-          <div>{this.getValue(product, 'color')}</div>
-          <div>{this.getValue(product, 'size')}</div>
-        </div>
+        {product && product.id && (
+          <>
+            <h1>{product.key}</h1>
+            <img src="#" alt={`The image of ${product.key}`} />
+            <ProductWrapper>
+              <ProductField>
+                <div className="label">SKU</div>
+                <div>{this.getValue(product, 'sku')}</div>
+              </ProductField>
+              <ProductField>
+                <div className="label">Description</div>
+                <div>{this.getValue(product, 'description')}</div>
+              </ProductField>
+              <ProductField>
+                <div className="label">Price</div>
+                <div>
+                  {formatPrice(
+                    this.getValue(product, 'price').value,
+                    this.getValue(product, 'price').unitAbbreviation
+                  )}
+                </div>
+              </ProductField>
+              <ProductField>
+                <div className="label">Color</div>
+                <div>{this.getValue(product, 'color')}</div>
+              </ProductField>
+              <ProductField>
+                <div className="label">Size</div>
+                <div>{this.getValue(product, 'size')}</div>
+              </ProductField>
+            </ProductWrapper>
+          </>
+        )}
       </>
     )
   }
